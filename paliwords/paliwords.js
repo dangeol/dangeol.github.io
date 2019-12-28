debc07efa6f2e46e289d54b11fb2d09b2563ab97
dangeol <do.dev@outlook.com>
2019-12-28 14:27:54 +0100

    feat: generic word search and layout changes

--- paliwords/paliwords.js
+++ paliwords/paliwords.js
@@ -1,49 +1,76 @@
+"use strict";
 var replMap = { 'ā':'a', 'ī':'i', 'ṅ':'n', 'ṇ':'n', 'ñ':'n', 'ū':'u' };
 var replRegex = new RegExp('[' + Object.keys(replMap).join('') + ']', 'ig');
+var paliWords, hits, hitsSorted, pedDescr, prev, notFoundWords, matchedPedWord, noOfWords;
 
 function replSpecialChars(input) {
   return replMap[input];
 }
 
-var ped = data[0].words;
+var ped = dataPed[0].words;
     
 const input = document.getElementById('userFile');
 const reader = new FileReader();
+
+$("#text,#translation,#glossary").click(function() {
+  var s = window.getSelection();
+  s.modify('extend', 'backward', 'word');
+  var b = s.toString();
+
+  s.modify('extend', 'forward', 'word');
+  var a = s.toString();
+  s.modify('move', 'forward', 'character');
+  getTranslation(b + a);
+  // credits to
+  // https://stackoverflow.com/users/762449/alphamale
+  // https://stackoverflow.com/users/2226755/a-312
+  // on https://stackoverflow.com/questions/8499865/get-a-word-by-single-click
+});
 
 input.onchange = function() {
   const file = input.files[0];
   reader.readAsText(file);
 };
 
+function getText() {
+  if (reader.result === null) {
+    alert("You must first select a file.");
+    return;
+  }
+  var text = reader.result;
+  document.getElementById("text").style.overflowY = "auto";
+  document.getElementById("text").innerHTML = text;
+
+  getPaliHitList();
+}
+
 function advancedSearch(ped, paliWordTemp) {
   var pedDescrTemp = ped.description.replace(replRegex, replSpecialChars);
   var advancedMatch = pedDescrTemp.toString().indexOf("**"+paliWordTemp+"**");
   if (advancedMatch !== -1) { 
-    pedDescrPrep = ped.description.substring(advancedMatch + 
+    matchedPedWord = ped.word;
+    var pedDescrPrep = ped.description.substring(advancedMatch + 
     paliWordTemp.length + 4, ped.description.length);
     return pedDescrPrep;
   }  
   return null;
 }
 
-function generateSheet() {
+function getPaliHitList() {
   if (reader.result === null) {
     alert("You must first select a file.");
     return;
   }
   document.getElementById("listOfWords").innerHTML = "";
-  const noOfWords = document.getElementById("sliderInput").value;
+  noOfWords = document.getElementById("sliderInput").value;
   var text = reader.result;
-  var words = text.match(/((?<=<i>))(.*?)(?=<\/i>)/g).toString().toLowerCase();
-  words = words.replace(/[;.:]/g,'');
-  words = words.replace(/(?<=,)\s+/g,'');
-  words = words.replace(/\(|\)|&nbsp|<br>/g,'');
-  words = words.replace(/ic(?=,|\s+)/g,'a');
-  words = words.replace(/s(?=,|\s+)/g,'');
-  words = words.replace(/\s+(?=,)/g,'');
-  const regex1 = /\s*,\s*|\s+/;
+  var words = text.match(/<i>(.*?)<\/i>/g).toString().toLowerCase();
+  words = words.replace(/\s+|<i>|<\/i>|[;.:]|\(|\)|&nbsp|<br>/g,'');
+  words = words.replace(/,\s+|\s+,|s,/g,',');
+  words = words.replace(/(ic|ically)(?=,|\s+)/g,'a');
+  const regex1 = /\s*,\s*|-|\s+/;
   var arrayOfWords = words.split(regex1);
-  var paliWords = [], hits = [], hitsSorted = [], pedDescr = [], prev, notFound = "";
+  paliWords = [], hits = [], hitsSorted = [], pedDescr = [], prev = "", notFoundWords = "", matchedPedWord = "";
   arrayOfWords.sort();
   for (var i = 0; i < arrayOfWords.length; i++) {
     if (arrayOfWords[i].substring(0,1).match(/[a-z]/) && arrayOfWords[i] !== prev) {
@@ -57,49 +84,14 @@ function generateSheet() {
   hitsSorted = hits.slice();
   hitsSorted.sort(function(a, b){return b - a});
   var thresholdHits = hitsSorted[noOfWords - 1];
-  //first pass: search in the json keys and in matching values
-  for (var i = 0; i < paliWords.length; i++) {
-    if (hits[i] >= thresholdHits) {
-      var j = 0;
-      var match = false;
-      pedDescr[i] = "(No translation was found.)";
-      var paliWordTemp = paliWords[i].toLowerCase().replace(replRegex, replSpecialChars);
-      while (j < ped.length && !match) {
-        var pedWord = ped[j].word.toLowerCase().replace(replRegex, replSpecialChars);
-        var pedWordSubstr1 = pedWord.substring(0, pedWord.length-1);
-        if (pedWord.localeCompare(paliWordTemp) === 0 || pedWordSubstr1.localeCompare(paliWordTemp) === 0) {
-          pedDescr[i] = ped[j].description;
-          match = true;
-        }
-        if (!match && (pedWord.substring(0, 3).localeCompare(paliWordTemp.substring(1, 4)) === 0)) { 
-          var advSearchResult = advancedSearch(ped[j], paliWordTemp);
-          if (advSearchResult !== null) {
-            pedDescr[i] = advSearchResult.substring(0, pedDescrPrep.indexOf("**")); 
-            match = true;    
-          }
-        }
-        j++;
-      } 
-    } if (!match) {
-      notFound = notFound.concat(paliWordTemp+",");
-    }         
-  }   
-  //second pass: in-depth search of the words that have not yet been found
-  for (var i = 0; i < paliWords.length; i++) {
-    if (notFound.indexOf(","+paliWords[i].toLowerCase().replace(replRegex, replSpecialChars)+",") !== -1) {
-      var j = 0;
-      var match = false;
-      var paliWordTemp = paliWords[i].toLowerCase().replace(replRegex, replSpecialChars);
-      while (j < ped.length && !match) {
-        var advSearchResult = advancedSearch(ped[j], paliWordTemp);
-        if (advSearchResult !== null) {
-          pedDescr[i] = advSearchResult.substring(0, pedDescrPrep.indexOf("**")); 
-          match = true;    
-        }
-        j++;
-      } 
-    }
-  }        
+
+  var hitsHeader = document.createElement("SPAN");
+  hitsHeader.innerText = "The top "+noOfWords+ " pāḷi words";
+  hitsHeader.style.fontSize = "18px";
+  hitsHeader.style.fontWeight = "bold";
+  hitsHeader.style.color = "red";
+  document.getElementById("listOfWords").appendChild(hitsHeader);
+
   paliWords.forEach(function(word, index) {
     if (hits[index] >= thresholdHits) {
       var li = document.createElement("li");
@@ -108,33 +100,84 @@ function generateSheet() {
       wordInput.style.fontSize = "18px";
       wordInput.style.fontWeight = "bold";
       var hitsInput = document.createElement("SPAN");
-      hitsInput.innerText = ' (' + hits[index] + ') : ';
+      hitsInput.innerText = ' (' + hits[index] + ')';
       hitsInput.style.fontSize = "18px";
       hitsInput.style.fontStyle = "italic";
       hitsInput.style.color = "blue";
-      var pedDescrInput = document.createElement("SPAN");
-      pedDescrInput.innerText = pedDescr[index].substring(0,180) + " [...]";
-      var buttonInput = document.createElement("INPUT");
-      var expanded = false;
-      buttonInput.type = "button";
-      buttonInput.value = "expand / collapse";
-      buttonInput.onclick = function() {
-        if (!expanded) {
-          pedDescrInput.innerText = pedDescr[index];
-          expanded = true;
-        } else {
-          pedDescrInput.innerText = pedDescr[index].substring(0,180) + " [...]";
-          expanded = false;
-        }
-      }
-      var newLine = document.createElement("SPAN");
-      newLine.innerHTML = "<br />";
       li.appendChild(wordInput);
       li.appendChild(hitsInput);
-      li.appendChild(buttonInput);
-      li.appendChild(newLine);
-      li.appendChild(pedDescrInput);
       document.getElementById("listOfWords").appendChild(li);
     }
   }); 
-};
+};  
+
+function getTranslation(paliWord) {
+  document.getElementById("translation").style.display = "block";
+  document.getElementById("translation").innerHTML = "";
+  paliWord = paliWord.toLowerCase();
+  paliWord = paliWord.replace(/(ic|ically)(?=,|\s+)/g,'a');
+  paliWord = paliWord.replace(/[;.:]|\s+|\(|\)|&nbsp|<br>/g,'');
+  matchedPedWord = "";
+  var notFound = false;
+  //first pass: search in the json keys and in matching values
+  var j = 0;
+  var match = false;
+  var paliWordTemp = paliWord.replace(replRegex, replSpecialChars);
+  while (j < ped.length && !match) {
+    var pedWord = ped[j].word.toLowerCase().replace(replRegex, replSpecialChars);
+    var pedWordSubstr1 = pedWord.substring(0, pedWord.length-1);
+    if (pedWord.localeCompare(paliWordTemp) === 0 
+      || /[0-9]/.test(pedWord.substring(pedWord.length - 1, pedWord.length)) 
+      && pedWordSubstr1.localeCompare(paliWordTemp) === 0) {
+      matchedPedWord = ped[j].word;
+      pedDescr = ped[j].description;
+      match = true;
+    }
+    if (!match && (pedWord.substring(0, 3).localeCompare(paliWordTemp.substring(1, 4)) === 0)) { 
+      var advSearchResult = advancedSearch(ped[j], paliWordTemp);
+      if (advSearchResult !== null) {
+        pedDescr = advSearchResult; 
+        match = true;    
+      }
+    }
+    j++;
+  } 
+  if (!match) {
+    notFound = true;
+  }         
+  //second pass: in-depth search of the words that have not yet been found
+  if (notFound) {
+    var j = 0;
+    var match = false;
+    var paliWordTemp = paliWord.replace(replRegex, replSpecialChars);
+    while (j < ped.length && !match) {
+      var advSearchResult = advancedSearch(ped[j], paliWordTemp);
+      if (advSearchResult !== null) {
+        pedDescr = advSearchResult; 
+        match = true;    
+      }
+      j++;
+    } 
+  }
+  var translDiv = document.createElement("DIV");
+  if (match) {
+    var wordInput = document.createElement("SPAN");
+    wordInput.innerText = paliWord;
+    wordInput.style.fontSize = "18px";
+    wordInput.style.fontWeight = "bold";
+    var pedDescrInput = document.createElement("SPAN");
+    pedDescrInput.innerText = pedDescr;
+    var newLine = document.createElement("SPAN");
+    newLine.innerHTML = "<br />";
+    translDiv.appendChild(wordInput);
+    translDiv.appendChild(newLine);
+    translDiv.appendChild(pedDescrInput);
+  } else {
+    var notFoundInput = document.createElement("SPAN");
+    notFoundInput.innerText = "No translation was found.";
+    notFoundInput.style.fontSize = "18px";
+    notFoundInput.style.fontWeight = "bold";
+    translDiv.appendChild(notFoundInput);
+  }  
+  document.getElementById("translation").appendChild(translDiv); 
+};  
